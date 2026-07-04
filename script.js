2c031a8ea72fa3d4447107a28ea92423c61779b0
TejaswiniDesai2006 <tejaswinidesai2006@gmail.com>
2026-07-04 20:46:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-async function getWeather(){
+async function searchMovie() {
 
-let city=document.getElementById("city").value;
+    let movie = document.getElementById("movie").value;
 
-let apiKey="7dc521dd518631e35e799fd3eeb0d3d5";
+    let apiKey = "415b3abd";
 
-let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let url = `https://www.omdbapi.com/?apikey=${apiKey}&t=${movie}`;
 
-let response=await fetch(url);
+    let response = await fetch(url);
 
-let data=await response.json();
+    let data = await response.json();
 
-document.getElementById("result").innerHTML=
-
-`
-
-<h2>${data.name}</h2>
-
-<p>Temperature : ${data.main.temp} °C</p>
-
-<p>Humidity : ${data.main.humidity}%</p>
-
-<p>${data.weather[0].description}</p>
-
-`;
-
+    document.getElementById("result").innerHTML = `
+        <h2>${data.Title}</h2>
+        <img src="${data.Poster}" width="200">
+        <p>Year : ${data.Year}</p>
+        <p>Genre : ${data.Genre}</p>
+        <p>IMDb : ${data.imdbRating}</p>
+    `;
 }
